aef227bc4aa2a46f1948a477be6739149e4f57df
Daniel C. Musigire <dalm05@cs.umb.edu>
2013-04-13 21:35:47 -0400

    first working form for products

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SmartBubble::Application.config.secret_token = 'ac07c6d6d175edb8cf73286174744bb4ca09ad9f715c217e01742e12a9550b4b289acd29a46a660e30ad050e9600742b3737f19056e466b76a1ce5c6ad4c74e4'
